49cc0cc6bbf530cd18dd34bfd970d2794aa9be52
MetalGearRay7 <raymondconnolly525@gmail.com>
2026-04-15 15:21:05 +0100

    fixed menu

--- code.js
+++ code.js
@@ -1,17 +1,12 @@
 function GoBOTW() {
 document.querySelector("#Start").classList.toggle("hidden");
-document.querySelector("#SnowMission").classList.toggle("hidden");
-document.querySelector("#JungleMission").classList.toggle("hidden");
 }
 
 function GoSB() {
 document.querySelector("#Start").classList.toggle("hidden");
-document.querySelector("#WildernessMission").classList.toggle("hidden");
-document.querySelector("#JungleMission").classList.toggle("hidden");
+
 }
 
 function GoRITJ() {
 document.querySelector("#Start").classList.toggle("hidden");
-document.querySelector("#WildernessMission").classList.toggle("hidden");
-document.querySelector("#SnowMission").classList.toggle("hidden");
 }
