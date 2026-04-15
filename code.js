56bbc89982741fef4de0985ce21535faa508e067
azel <adamplota77@gmail.com>
2026-04-15 14:09:49 +0100

    code

---
+++ code.js
@@ -0,0 +1,14 @@
+//let CurrentArea = document.querySelector("#Start");
+let GoldCoins;
+
+function GoCozyCabin()
+{
+    document.querySelector("#StartSnowMountain").classList.toggle("hidden");
+    document.querySelector("#CozyCabin").classList.toggle("hidden");
+}
+
+function GoMountain()
+{
+    document.querySelector("#StartSnowMountain").classList.toggle("hidden");
+    document.querySelector("#Mountain").classList.toggle("hidden");
+}
