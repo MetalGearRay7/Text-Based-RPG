54fd3f13550c359e25bfcce9b90a509f5d7c1328
MetalGearRay7 <raymondconnolly525@gmail.com>
2026-04-17 12:43:28 +0100

    added more to area 1

--- code.js
+++ code.js
@@ -2,7 +2,6 @@ function GoBOTW() {
 document.querySelector("#Menu-Start").classList.toggle("hidden");
 document.querySelector("#Adam-Start").classList.remove("hidden");
 }
-
 function GoSB() {
 document.querySelector("#Menu-Start").classList.toggle("hidden");
 document.querySelector("#Jacob-Start").classList.remove("hidden");
@@ -13,7 +12,26 @@ document.querySelector("#Menu-Start").classList.toggle("hidden");
 document.querySelector("#Ray-Start").classList.remove("hidden");
 }
 
-function FollowTheMap() {
-document.querySelector("#Follow-Map").classList.remove("hidden");
+function FollowTheMap1() {
 document.querySelector("#Start").classList.toggle("hidden");
+document.querySelector("#Follow-Map").classList.remove("hidden");
+}
+
+function StayPut() {
+document.querySelector("#Start").classList.toggle("hidden");
+document.querySelector("#Stay-Put").classList.remove("hidden");
+
+}
+function StayLonger() {
+document.querySelector("#Stay-Put").classList.remove("hidden");
+}
+function FollowTheMap2() {
+document.querySelector("#Stay-Put").classList.toggle("hidden");
+document.querySelector("#Follow-Map").classList.remove("hidden");
+}
+function Continue1() {
+document.querySelector("#Continue2").classList.remove("hidden");
+}
+function Continue2() {
+document.querySelector("#Continue3").classList.remove("hidden");
 }
