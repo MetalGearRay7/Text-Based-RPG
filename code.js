9f97d38a74c3d04fe27605fe85720d0c27e3f17b
azel <adamplota77@gmail.com>
2026-04-17 10:13:26 +0100

    changed stuff

--- code.js
+++ code.js
@@ -3,39 +3,44 @@ let GoldCoins;
 
 function GoRuinedTower()
 {
-    document.querySelector("#StartLakeside").classList.toggle("hidden");
     document.querySelector("#RuinedTower").classList.toggle("hidden");
+    document.querySelector("#StartLakeside").classList.toggle("hidden");
 }
 
 function GoCampsite()
 {
-    document.querySelector("#StartLakeside").classList.toggle("hidden");
     document.querySelector("#Campsite").classList.toggle("hidden");
+    document.querySelector("#StartLakeside").classList.toggle("hidden");
 }
 
-function GoOpenTowerChest()
+function OpenTowerChest()
 {
-    document.querySelector("#GoOpenTowerChest").classList.toggle("hidden");
+    document.querySelector("#OpenTowerChest").classList.toggle("hidden");
+    document.querySelector("#RuinedTower").classList.toggle("hidden");
 }
+
 
 function LeaveTower()
 {
     document.querySelector("#LeaveTower").classList.toggle("hidden");
-    document.querySelector("#StartLakeside").classList.toggle("hidden");
+    document.querySelector("#RuinedTower").classList.toggle("hidden");
 }
 
 function CampRelax()
 {
     document.querySelector("#CampRelax").classList.toggle("hidden");
+    document.querySelector("#Campsite").classList.toggle("hidden");
 }
 
 function StealCampSupplies()
 {
     document.querySelector("#StealCampSupplies").classList.toggle("hidden");
+    document.querySelector("#Campsite").classList.toggle("hidden");
+    document.querySelector("#CampRelax").classList.toggle("hidden");
 }
 
 function LeaveCampsite()
 {
     document.querySelector("#LeaveCampsite").classList.toggle("hidden");
-    document.querySelector("#StartLakeside").classList.toggle("hidden");
+    document.querySelector("#Campsite").classList.toggle("hidden");
 }
